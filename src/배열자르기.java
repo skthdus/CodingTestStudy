98360eb52441374ce8256aaa14277241e9369f35
skthdus <ssoy617@gmail.com>
2023-04-05 18:16:10 +0900

    20230405

---
+++ src/배열자르기.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class 배열자르기 {
+
+    public int[] solution(int[] numbers, int num1, int num2) {
+
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
+
+    }
+
+}
