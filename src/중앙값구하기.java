8983866e6731ef707cd4171cb5e9ae2cb4b188b9
skthdus <ssoy617@gmail.com>
2023-04-09 19:41:17 +0900

    20230409

---
+++ src/중앙값구하기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class 중앙값구하기 {
+
+    public int solution(int[] array) {
+        int answer = 0;
+
+        // 배열 정렬
+        Arrays.sort(array);
+
+        answer = array[array.length/2];
+
+        return answer;
+    }
+
+}
---
+++ src/편지.java
@@ -0,0 +1,11 @@
+public class 편지 {
+
+    public int solution(String message) {
+        int answer = 0;
+
+        answer = message.length() * 2;
+
+        return answer;
+    }
+
+}
