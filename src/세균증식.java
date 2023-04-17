8c034c27d3c3f8bd318965649f00e737d9beb01b
skthdus <ssoy617@gmail.com>
2023-04-17 19:09:46 +0900

    20230417

---
+++ src/세균증식.java
@@ -0,0 +1,14 @@
+public class 세균증식 {
+
+    public int solution(int n, int t) {
+
+        int answer = n;
+
+        for(int i =1; i <= t; i++){
+            answer *=2;
+        }
+
+        return answer;
+    }
+
+}
