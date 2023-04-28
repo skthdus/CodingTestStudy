808cd461d4fb936eee6b3fdcbf2b6ca36cf4d19c
skthdus <ssoy617@gmail.com>
2023-04-28 12:11:21 +0900

    20230428

---
+++ src/중복된문자제거.java
@@ -0,0 +1,15 @@
+public class 중복된문자제거 {
+
+    public String solution(String my_string) {
+        String answer = "";
+
+        for(int i = 0; i < my_string.length(); i++){
+            if(!answer.contains(String.valueOf(my_string.charAt(i)))){
+                answer += my_string.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+}
