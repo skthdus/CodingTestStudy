7e66f7a15639da20de0e706187d74381f7d083ee
skthdus <ssoy617@gmail.com>
2023-04-30 15:26:14 +0900

    20230430

---
+++ src/약수구하기.java
@@ -0,0 +1,27 @@
+public class 약수구하기 {
+
+    public int[] solution(int n) {
+
+        int count = 0;
+
+        for(int i=1; i<=n ; i++){
+            if (n % i == 0){
+                count++;
+            }
+        }
+
+        int[] answer = new int [count];
+
+        int j = 0;
+
+        for(int i=1; i<=n; i++){
+            if(n % i ==0){
+                answer[j] = i;
+                j++;
+            }
+        }
+
+        return answer;
+    }
+
+}
