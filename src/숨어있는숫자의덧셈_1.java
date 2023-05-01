1826e021ca103beea235462216102d7381db879d
skthdus <ssoy617@gmail.com>
2023-05-01 11:11:11 +0900

    20230501

---
+++ src/숨어있는숫자의덧셈_1.java
@@ -0,0 +1,16 @@
+public class 숨어있는숫자의덧셈_1 {
+
+    public int solution(String my_string) {
+        int answer = 0;
+
+        String str = my_string.replaceAll("[^0-9]", "");
+        String[] arr = str.split("");
+
+        for(int i = 0; i<arr.length; i++ ){
+            answer += Integer.parseInt(arr[i]);
+        }
+
+        return answer;
+    }
+
+}
