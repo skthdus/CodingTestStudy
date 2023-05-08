080a8c7c0d8a0c8408413af22471de4fc29c7363
skthdus <ssoy617@gmail.com>
2023-05-08 14:29:05 +0900

    20230508

---
+++ src/팩토리얼.java
@@ -0,0 +1,21 @@
+public class 팩토리얼 {
+
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            if (n >= factorial(i)) {
+                answer = i;
+            } else {
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static int factorial(int number) {
+        if (number > 1) return number * factorial(number - 1);
+        return number;
+    }
+
+}
